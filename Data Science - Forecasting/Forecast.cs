65964b0c4420e2d5c8d8c37d5c63039f5266da1f
DESKTOP-C809P49\KevinPC <0889851@hr.nl>
2017-03-29 17:52:10 +0200

    Started with computing error

---
+++ Data Science - Forecasting/Forecast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Science___Forecasting
+{
+    class Forecast
+    {
+        public void calcSES(List<DemandPoint> demandList)
+        {
+            List<DemandPoint> optimalValue = new List<DemandPoint>();
+
+            float minError = float.PositiveInfinity;
+            float optimalA = 1;
+
+            float alpha = 0.01f;
+            while (alpha < 1)
+            {
+                List<DemandPoint> demandPoints = new List<DemandPoint>();
+                demandPoints.Add(demandList[0]);
+            }
+        }
+    }
+}
+
+        
